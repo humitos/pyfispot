62d5cf7c4efaed3654d8a93408429839f97b1a49
Manuel Kaufmann <humitos@gmail.com>
2015-10-10 14:22:43 -0500

    Tools updated

---
+++ utils/osm_maps_download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Download maps for OSMAnd for Android
+wget -c "http://download.osmand.net/download.php?standard=yes&file=Peru_southamerica_2.obf.zip" -O Peru_southamerica_2.obf.zip
+wget -c "http://download.osmand.net/download.php?standard=yes&file=Bolivia_southamerica_2.obf.zip" -O Bolivia_southamerica_2.obf.zip
