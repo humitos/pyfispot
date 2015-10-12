5091a472b4cd327d5ba5a18502be8aac70275096
Manuel Kaufmann <humitos@gmail.com>
2015-10-11 19:43:23 -0500

    Initial index design by DesignBootstrp

---
+++ raspberrypi/home/pi/apps/pyfispot/static/js/custom-solid.js
@@ -0,0 +1,97 @@
+/*=============================================================
+    Authour URL: www.designbootstrap.com
+    
+    http://www.designbootstrap.com/
+
+    License: MIT
+
+    http://opensource.org/licenses/MIT
+
+    100% Free To use For Personal And Commercial Use.
+
+    IN EXCHANGE JUST TELL PEOPLE ABOUT THIS WEBSITE
+   
+========================================================  */
+
+$(document).ready(function () {
+
+/*====================================
+SCROLLING SCRIPTS
+======================================*/
+
+$('.scroll-me a').bind('click', function (event) { //just pass scroll-me in design and start scrolling
+var $anchor = $(this);
+$('html, body').stop().animate({
+scrollTop: $($anchor.attr('href')).offset().top
+}, 1200, 'easeInOutExpo');
+event.preventDefault();
+});
+
+
+/*====================================
+SLIDER SCRIPTS
+======================================*/
+
+
+$('#carousel-slider').carousel({
+interval: 2000 //TIME IN MILLI SECONDS
+});
+
+
+/*====================================
+POPUP IMAGE SCRIPTS
+======================================*/
+$('.fancybox-media').fancybox({
+openEffect: 'elastic',
+closeEffect: 'elastic',
+helpers: {
+title: {
+type: 'inside'
+}
+}
+});
+
+
+/*====================================
+FILTER FUNCTIONALITY SCRIPTS
+======================================*/
+$(window).load(function () {
+var $container = $('#work-div');
+$container.isotope({
+filter: '*',
+animationOptions: {
+duration: 750,
+easing: 'linear',
+queue: false
+}
+});
+$('.caegories a').click(function () {
+$('.caegories .active').removeClass('active');
+$(this).addClass('active');
+var selector = $(this).attr('data-filter');
+$container.isotope({
+filter: selector,
+animationOptions: {
+duration: 750,
+easing: 'linear',
+queue: false
+}
+});
+return false;
+});
+
+});
+
+
+
+/*====================================
+WRITE YOUR CUSTOM SCRIPTS BELOW
+======================================*/
+
+
+
+
+
+
+
+});
---
+++ raspberrypi/home/pi/apps/pyfispot/static/js/custom.js
@@ -0,0 +1,111 @@
+/*=============================================================
+    Authour URL: www.designbootstrap.com
+    
+    http://www.designbootstrap.com/
+
+    License: MIT
+
+    http://opensource.org/licenses/MIT
+
+    100% Free To use For Personal And Commercial Use.
+
+    IN EXCHANGE JUST TELL PEOPLE ABOUT THIS WEBSITE
+   
+========================================================  */
+
+$(document).ready(function () {
+
+/*====================================
+SCROLLING SCRIPTS
+======================================*/
+
+$('.scroll-me a').bind('click', function (event) { //just pass scroll-me in design and start scrolling
+var $anchor = $(this);
+$('html, body').stop().animate({
+scrollTop: $($anchor.attr('href')).offset().top
+}, 1200, 'easeInOutExpo');
+event.preventDefault();
+});
+
+
+/*====================================
+SLIDER SCRIPTS
+======================================*/
+
+
+$('#carousel-slider').carousel({
+interval: 2000 //TIME IN MILLI SECONDS
+});
+
+
+/*====================================
+VAGAS SLIDESHOW SCRIPTS
+======================================*/
+$.vegas('slideshow', {
+backgrounds: [
+{ src: '/static/img/1.jpg', fade: 1000, delay: 9000 },
+{ src: '/static/img/2.jpg', fade: 1000, delay: 9000 },
+]
+})('overlay', {
+/** SLIDESHOW OVERLAY IMAGE **/
+src: '/static/js/vegas/overlays/06.png' // THERE ARE TOTAL 01 TO 15 .png IMAGES AT THE PATH GIVEN, WHICH YOU CAN USE HERE
+});
+
+
+/*====================================
+POPUP IMAGE SCRIPTS
+======================================*/
+$('.fancybox-media').fancybox({
+openEffect: 'elastic',
+closeEffect: 'elastic',
+helpers: {
+title: {
+type: 'inside'
+}
+}
+});
+
+
+/*====================================
+FILTER FUNCTIONALITY SCRIPTS
+======================================*/
+$(window).load(function () {
+var $container = $('#work-div');
+$container.isotope({
+filter: '*',
+animationOptions: {
+duration: 750,
+easing: 'linear',
+queue: false
+}
+});
+$('.caegories a').click(function () {
+$('.caegories .active').removeClass('active');
+$(this).addClass('active');
+var selector = $(this).attr('data-filter');
+$container.isotope({
+filter: selector,
+animationOptions: {
+duration: 750,
+easing: 'linear',
+queue: false
+}
+});
+return false;
+});
+
+});
+
+
+
+/*====================================
+WRITE YOUR CUSTOM SCRIPTS BELOW
+======================================*/
+
+
+
+
+
+
+
+});
