ca44fefe2039907f4c0b4a82d99d262d97c720f2
Manuel Kaufmann <humitos@gmail.com>
2015-10-11 20:20:06 -0500

    Script to update our blogs into the RaspberryPi

---
+++ utils/update_blogs.sh
@@ -0,0 +1,2 @@
+rsync -rav --delete-after --copy-links ~/Source/blog/nikola/output/* pi@raspberrypi.redlibre:~/apps/elblogdehumitos.com.ar
+rsync -rav --delete-after --copy-links ~/Source/argentinaenpython.com.ar/web/output/* pi@raspberrypi.redlibre:~/apps/argentinaenpython.com.ar
