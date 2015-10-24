a6ef752a7930c6c4de0af23a63d49d33c12a8813
Manuel Kaufmann <humitos@gmail.com>
2015-10-24 09:34:47 -0500

    Download more maps for OSMAnd

--- utils/osm_maps_download.sh
+++ utils/osm_maps_download.sh
@@ -3,3 +3,9 @@
 # Download maps for OSMAnd for Android
 wget -c "http://download.osmand.net/download.php?standard=yes&file=Peru_southamerica_2.obf.zip" -O Peru_southamerica_2.obf.zip
 wget -c "http://download.osmand.net/download.php?standard=yes&file=Bolivia_southamerica_2.obf.zip" -O Bolivia_southamerica_2.obf.zip
+wget -c "http://download.osmand.net/download.php?standard=yes&file=Ecuador_southamerica_2.obf.zip" -O Ecuador_southamerica_2.obf.zip
+
+# Extras
+wget -c "http://download.osmand.net/download.php?standard=yes&file=World_altitude_correction_WW15MGH.extra.zip" -O World_altitude_correction_WW15MGH.extra.zip
+wget -c "http://download.osmand.net/download.php?standard=yes&file=World_basemap_2.obf.zip" -O World_basemap_2.obf.zip
+wget -c "http://download.osmand.net/download.php?standard=yes&file=World_seamarks_basemap_2.obf.zip" -O World_seamarks_basemap_2.obf.zip
