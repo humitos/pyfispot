be87303f3f56421f338e88b8fd272614ea0dc1e8
Manuel Kaufmann <humitos@gmail.com>
2015-10-27 15:53:37 -0500

    More maps for OSM

--- utils/osm_maps_download.sh
+++ utils/osm_maps_download.sh
@@ -4,8 +4,16 @@
 wget -c "http://download.osmand.net/download.php?standard=yes&file=Peru_southamerica_2.obf.zip" -O Peru_southamerica_2.obf.zip
 wget -c "http://download.osmand.net/download.php?standard=yes&file=Bolivia_southamerica_2.obf.zip" -O Bolivia_southamerica_2.obf.zip
 wget -c "http://download.osmand.net/download.php?standard=yes&file=Ecuador_southamerica_2.obf.zip" -O Ecuador_southamerica_2.obf.zip
+wget -c "http://download.osmand.net/download.php?standard=yes&file=Chile_southamerica_2.obf.zip" -O Chile_southamerica_2.obf.zip
 
 # Extras
 wget -c "http://download.osmand.net/download.php?standard=yes&file=World_altitude_correction_WW15MGH.extra.zip" -O World_altitude_correction_WW15MGH.extra.zip
 wget -c "http://download.osmand.net/download.php?standard=yes&file=World_basemap_2.obf.zip" -O World_basemap_2.obf.zip
 wget -c "http://download.osmand.net/download.php?standard=yes&file=World_seamarks_basemap_2.obf.zip" -O World_seamarks_basemap_2.obf.zip
+
+
+# Download maps for Maps.ME
+wget -c http://direct.mapswithme.com/direct/latest/Peru.mwm.routing
+wget -c http://direct.mapswithme.com/direct/latest/Chile.mwm.routing
+wget -c http://direct.mapswithme.com/direct/latest/Ecuador.mwm.routing
+wget -c http://direct.mapswithme.com/direct/latest/Bolivia.mwm.routing
