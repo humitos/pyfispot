16f8167a081e60c8486376388ea2576b2f534e04
Manuel Kaufmann <humitos@gmail.com>
2016-01-02 14:56:03 -0500

    Script to be ran in the RaspberryPi to download all the tutorials

---
+++ utils/update_tutorials.sh
@@ -0,0 +1,36 @@
+set -e
+
+cd ~/apps
+rm -rf tutorial.djangogirls.org docs.python.org.ar djangotutorial.readthedocs.org
+
+# Tutorial Django Girls (Ingles)
+wget --continue --recursive \
+     --no-clobber --page-requisites \
+     --html-extension --convert-links \
+     --restrict-file-names=windows \
+     --no-parent \
+     http://tutorial.djangogirls.org/en/index.html
+
+# Tutorial Django Girls (Español)
+wget --continue --recursive \
+     --no-clobber --page-requisites \
+     --html-extension --convert-links \
+     --restrict-file-names=windows \
+     --no-parent \
+     http://tutorial.djangogirls.org/es/index.html
+
+# Tutorial Python
+wget --continue --recursive \
+     --no-clobber --page-requisites \
+     --html-extension --convert-links \
+     --restrict-file-names=windows \
+     --no-parent \
+     http://docs.python.org.ar/tutorial/3/index.html
+
+# Tutorial Django
+wget --continue --recursive \
+     --no-clobber --page-requisites \
+     --html-extension --convert-links \
+     --restrict-file-names=windows \
+     --no-parent \
+    http://djangotutorial.readthedocs.org/es/1.8/
